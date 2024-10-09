a301128c84cdef3e4a6cf2a63a2814dde55d739f
dmozgiel <darius@mozgiel.de>
2024-10-09 13:30:38 +0200

    [FEATURE]: Added form page selection to show loading spinner only on certain pages

--- moz-loadingspinner/dist/Frontend.js
+++ moz-loadingspinner/dist/Frontend.js
@@ -16,18 +16,27 @@
 
       let _this = this;
       this.selector = null;
+      this.pages = null;
+      this.currentPage = null;
       this.wasClicked = false;
 
-      this.init = function( selector ) {
+      this.init = function( selector, pages ) {
 
         this.selector = selector;
-
+        // pages is a comma separated list of page ids, example: 1,2,3 explode it to an array
+        this.pages = pages.split(',');
+        console.log( this.pages );
         console.log('Init MozLoadingSpinner');
         console.log(this.selector);
       }
 
       $(selector + ' button[type="submit"]').on('click', function( e ) {
-        console.log('clicked');
+        let value = $(this).val();
+        // if value and if pages is set and if value is in pages
+        if( _this.pages && ! _this.pages.includes(value) ) {
+          return;
+        }
+
         if( _this.wasClicked ) {
           return;
         }
@@ -41,8 +50,9 @@
     }
 
     var selector = $('#MozLsSelector').data('selector');
+    var pages = $('#MozLsSelector').data('pages');
     let mozLoadingSpinner = new MozLoadingSpinner();
-    mozLoadingSpinner.init( selector );
+    mozLoadingSpinner.init( selector, pages );
 
   });
 
